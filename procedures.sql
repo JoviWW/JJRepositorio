c735ffb2b6cd0a9e90f17737b3479cd0fbf0ead1
JoviWW <josevww@gmail.com>
2022-06-13 22:45:38 -0300

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -15,7 +15,7 @@ END $
 DELIMITER ;
 
 DELIMITER $
-CREATEPROCEDURE `ConfirmarCompra`(IN `codigoBaixa` INT, IN `incomprador` VARCHAR(55), IN `invalorfinal` FLOAT)
+CREATE PROCEDURE `ConfirmarCompra`(IN `codigoBaixa` INT, IN `incomprador` VARCHAR(55), IN `invalorfinal` FLOAT)
 BEGIN
 UPDATE operacao SET data_finalBaixa = CURRENT_TIMESTAMP(), comprador = incomprador, valorfinal = invalorfinal, concluida = 1 WHERE codigo = codigoBaixa;
 UPDATE faca f inner join operacao o on(o.codigoFaca = f.codigo) SET estoqueprocessamento = estoqueprocessamento-1 WHERE o.codigo = codigoBaixa;
